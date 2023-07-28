f24eca42f2733bbec88db64da95a4df8507f0d7d
Magila-Ferreira <magilamorganasf@hotmail.com>
2023-07-28 00:40:11 -0300

    Projeto finalizado com #Felipe Aguiar

--- src/app/components/amount-box/amount-box.component.ts
+++ src/app/components/amount-box/amount-box.component.ts
@@ -2,26 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { AmountDataModel } from 'src/app/model/amountDataModel';
 import { AccountsService } from 'src/app/services/accounts.service';
 @Component({
-  selector: 'app-amount-box',
-  templateUrl: './amount-box.component.html',
-  styleUrls: ['./amount-box.component.css']
+	selector: 'app-amount-box',
+	templateUrl: './amount-box.component.html',
+	styleUrls: ['./amount-box.component.css'],
 })
 export class AmountBoxComponent implements OnInit {
 	accountDetails: AmountDataModel = {
 		amountValue: 0,
 		limit: 0,
-		totalAmount: 0
-	}
+		totalAmount: 0,
+	};
 
-	constructor(private service: AccountsService) {
+	constructor(private service: AccountsService) {}
 
-	}
 	ngOnInit(): void {
 		this.getAmountValues();
 	}
 
 	getAmountValues() {
-		this.service.get // Continuar em 2:49:00
+		this.service.getAccount().subscribe(data => {
+			this.accountDetails.amountValue = data.account.balance;
+			this.accountDetails.limit = data.account.limit;
+			this.accountDetails.totalAmount =
+				this.accountDetails.amountValue + this.accountDetails.limit;
+		});
 	}
-
 }
